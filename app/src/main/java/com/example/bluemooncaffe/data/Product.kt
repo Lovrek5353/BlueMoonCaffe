120454626421f08fef3e33ad2c3c2c0107225c37
Lovro <paladinic.l@gmail.com>
2023-04-07 21:01:35 +0200

    Added basic packages and stuff. Add Firestore config file

---
+++ app/src/main/java/com/example/bluemooncaffe/data/Product.kt
@@ -0,0 +1,10 @@
+package com.example.bluemooncaffe.data
+
+data class Product(
+    var id: Int = 0,
+    var categoryId: Int = 0,
+    var name: String = "",
+    var price: Double = 0.0,
+    var servingSize: Double = 0.0,
+    var imageLink: String=""
+)
